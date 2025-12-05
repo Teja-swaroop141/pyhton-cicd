ea734bb27e3f906e6bdbce3e008c2d44a1d77726
Teja <steja9748@gmail.com>
2025-12-05 20:11:32 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+todos = []
+
+@app.route('/')
+def home():
+    return "To-Do App Backend Running!"
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.get_json()
+    todos.append(data)
+    return jsonify({"message": "Todo added!", "todos": todos}), 201
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
